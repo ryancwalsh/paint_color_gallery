25655046c33f7cbac8d3a7d463de536df1a66d77
Ryan Walsh <rcwalsh@gmail.com>
2022-10-02 11:13:58 -0400

    sort by hue

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,8 @@ import styles from '../styles/Home.module.scss';
 const colornerdDir = './node_modules/colornerd';
 const dir = `${colornerdDir}/_dev/`;
 
+type ColorObj = any;
+
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
   // https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#getserversideprops-with-typescript
   const bookFileDetailObjects = JSON.parse(fs.readFileSync(`${dir}books.json`, 'utf8'));
@@ -17,16 +19,20 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
   const books: any = {};
   bookFileDetailObjects.forEach(function (bookFileDetailObject: any) {
     const filename = `${colornerdDir}/json/${bookFileDetailObject.filename}.json`;
-    const records = JSON.parse(fs.readFileSync(`${filename}`, 'utf8'));
-    // console.log({ records });
-    books[filename] = records;
+    // console.log({ filename });
+    if (filename === './node_modules/colornerd/json/sherwin-williams.json') {
+      // TODO Don't filter to filename in a hard-coded way.
+      const records = JSON.parse(fs.readFileSync(`${filename}`, 'utf8'));
+      // console.log({ records });
+      books[filename] = records;
+    }
   });
   return {
     props: { books }, // will be passed to the page component as props
   };
 };
 
-function ColorCell({ index, color }: any): JSX.Element {
+function getHsl(color: ColorObj) {
   let hsl;
   try {
     const colorObj = colorLib(color.hex);
@@ -34,15 +40,44 @@ function ColorCell({ index, color }: any): JSX.Element {
   } catch (error) {
     console.error({ error, color });
   }
+  return hsl;
+}
+
+function getHue(color: ColorObj) {
+  let hue;
+  try {
+    const colorObj = colorLib(color.hex);
+    hue = colorObj.hue();
+  } catch (error) {
+    console.error({ error, color });
+  }
+  return hue;
+}
+
+function sortArray(array: ColorObj[], compareValueGetter: any) {
+  // https://stackoverflow.com/a/54383087/470749 and I should consider https://tomekdev.com/posts/sorting-colors-in-js too
+  array.sort((elem1, elem2) => {
+    const value1 = compareValueGetter(elem1);
+    const value2 = compareValueGetter(elem2);
+    // eslint-disable-next-line no-nested-ternary
+    return value1 > value2 ? 1 : value1 < value2 ? -1 : 0;
+  });
+}
+
+function ColorCell({ index, color }: any): JSX.Element {
+  const hsl = getHsl(color);
 
   return (
-    <div key={index} style={{ background: hsl, padding: '1rem' }}>
+    <div key={index} style={{ background: hsl, padding: '0.5rem', display: 'inline-block', width: '100px', height: '100px' }}>
       {color.name}
     </div>
   );
 }
 function ColorBook({ index, books, filename }: any): JSX.Element {
-  const colorsInBook = books[filename];
+  const colorsInBook = books[filename].map((color: ColorObj) => {
+    return { ...color, hsl: getHsl(color), hue: getHue(color) };
+  });
+  sortArray(colorsInBook, (color: ColorObj) => color.hue);
   return (
     <div key={index}>
       <h2>{filename}</h2>
