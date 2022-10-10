cae227ef0a953b159c665e7505e28593c9ae7630
Ryan Walsh <rcwalsh@gmail.com>
2022-10-10 17:16:11 -0400

    re-added ColorCell

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,25 @@ import megaColors from '../data/colornerd.json';
 import { getFilteredColors } from '../helpers/colors';
 import useLocalStorage from '../helpers/useLocalStorage';
 import styles from '../styles/Home.module.scss';
+import { MegaColor } from '../types';
+
+function ColorCell({ megaColor }: { megaColor: MegaColor }): JSX.Element {
+  // console.log({ megaColor });
+  const { colorDetailsObject } = megaColor;
+
+  return (
+    <div style={{ background: colorDetailsObject.hsl(), display: 'inline-block', height: '100px', margin: '2px', padding: '0.5rem', width: '100px' }}>
+      <div className="colorName">{megaColor.name}</div>
+      <div className="book" style={{ fontSize: '0.8rem' }}>
+        {megaColor.book}
+      </div>
+    </div>
+  );
+}
 
 function TaskList(): JSX.Element {
+  // TODO: https://stackoverflow.com/questions/74020543/how-to-disable-prettier-for-pre-or-code-blocks-so-that-new-lines-line-break
+  // prettier-ignore-start
   return (
     <pre style={{ maxWidth: '900px', overflow: 'auto' }}>
       TODO: - get color wheel working - fix types in VSC - deploy to GH Pages - support loading new JSON book to localStorage - filter which books to include - allow clustering by
@@ -15,6 +32,7 @@ function TaskList(): JSX.Element {
       Analytics - get a URL
     </pre>
   );
+  // prettier-ignore-end
 }
 
 function Sliders({ toleranceH, setToleranceH, toleranceS, setToleranceS, toleranceL, setToleranceL }: any): JSX.Element {
@@ -42,6 +60,7 @@ const Home: NextPage = () => {
   const [toleranceH, setToleranceH] = useLocalStorage<number>('toleranceH', 3);
   const [toleranceS, setToleranceS] = useLocalStorage<number>('toleranceS', 3);
   const [toleranceL, setToleranceL] = useLocalStorage<number>('toleranceL', 3);
+  // console.log({ toleranceH, toleranceL, toleranceS });
 
   const results = getFilteredColors(targetColor, megaColors, toleranceH, toleranceS, toleranceL);
   console.log({ results });
@@ -51,7 +70,11 @@ const Home: NextPage = () => {
 
       <Sliders {...{ setToleranceH, setToleranceL, setToleranceS, toleranceH, toleranceL, toleranceS }} />
       <div style={{ backgroundColor: targetColor }}>
-        <div className="colors"></div>
+        <div className="colors">
+          {results.map((colorInMap: MegaColor) => (
+            <ColorCell key={`${colorInMap.book}_${colorInMap.code}`} megaColor={colorInMap} />
+          ))}
+        </div>
         <TaskList />
       </div>
     </Layout>
