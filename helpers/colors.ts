ca3499f0efe0873d11947070c610a3ee65071b5d
Ryan Walsh <rcwalsh@gmail.com>
2022-10-10 16:07:39 -0400

    delete unused color sorting code

--- helpers/colors.ts
+++ helpers/colors.ts
@@ -32,59 +32,6 @@ for (let index = 0; index < numberClusters; index += 1) {
   });
 }
 
-function colorDistance(a: ColorLibraryObject, b: ColorLibraryObject): number {
-  const x = (a.hue() - b.hue()) ** 2 + (a.saturationl() - b.saturationl()) ** 2 + (a.lightness() - b.lightness()) ** 2;
-  return Math.sqrt(x);
-}
-
-function getSortValue(color: ColorLibraryObject): number {
-  return color.lightness() * 5 + color.saturationl() * 2 + color.hue();
-}
-
-function sortArray(array: MegaColor[]) {
-  // https://stackoverflow.com/a/54383087/470749 and I should consider https://tomekdev.com/posts/sorting-colors-in-js too
-  array.sort((element1, element2) => {
-    // const value1 = elem1.colorLibObject.hue();
-    // const value2 = elem2.colorLibObject.hue();
-    // const colorToCompareTo = colorLib('#000000');
-    // const value1 = colorDistance(elem1.colorLibObject, colorToCompareTo);
-    // const value2 = colorDistance(elem2.colorLibObject, colorToCompareTo);
-    const value1 = getSortValue(element1.colorLibObject);
-    const value2 = getSortValue(element2.colorLibObject);
-
-    if (value1 > value2) return 1;
-    else if (value1 < value2) return -1;
-    else return 0;
-  });
-}
-
-export function sortWithClusters(colorsToSort: MegaColor[]) {
-  // See https://tomekdev.com/posts/sorting-colors-in-js and https://codepen.io/tniezurawski/pen/zYZQRvG?editors=0010
-  for (const megaColor of colorsToSort) {
-    let minDistance: number | null = null;
-    let minDistanceClusterIndex = 0;
-
-    for (const [clusterIndex, cluster] of clusters.entries()) {
-      const distance = colorDistance(megaColor.colorLibObject, cluster.leadColor);
-      if (minDistance === null || minDistance > distance) {
-        minDistance = distance;
-        minDistanceClusterIndex = clusterIndex;
-      }
-    }
-
-    clusters[minDistanceClusterIndex].colors.push(megaColor);
-  }
-
-  for (const cluster of clusters) {
-    const { colors } = cluster;
-    sortArray(colors);
-    // eslint-disable-next-line no-param-reassign
-    cluster.colors = colors;
-  }
-
-  return clusters;
-}
-
 function isHueWithinTolerance(hue: number, hueToMatch: number, percentageTolerance: number): boolean {
   // TODO: Does this properly handle hue wrap-around?
   const degreeTolerance = (360 / 100) * percentageTolerance;
