34597bdeee3b6b3756f5631d652d6efe647f70a6
Ryan Walsh <rcwalsh@gmail.com>
2022-10-16 11:05:09 -0400

    styles

--- components/Sliders.tsx
+++ components/Sliders.tsx
@@ -1,17 +1,26 @@
 export default function Sliders({ toleranceH, setToleranceH, toleranceS, setToleranceS, toleranceL, setToleranceL }: any): JSX.Element {
   return (
-    <div>
+    <div className="sliders">
       <label>
-        <input type="range" defaultValue={toleranceH} onChange={(event) => setToleranceH(Number(event.target.value))} min="0" max="50" step="1" />
-        {toleranceH}
+        <div className="label">Hue:</div>
+        <div className="flex">
+          <span className="currentValue">{toleranceH}%</span>
+          <input type="range" defaultValue={toleranceH} onChange={(event) => setToleranceH(Number(event.target.value))} min="0" max="50" step="1" />
+        </div>
       </label>
       <label>
-        <input type="range" defaultValue={toleranceS} onChange={(event) => setToleranceS(Number(event.target.value))} min="0" max="50" step="1" />
-        {toleranceS}
+        <div className="label">Saturation:</div>
+        <div className="flex">
+          <span className="currentValue">{toleranceS}%</span>
+          <input type="range" defaultValue={toleranceS} onChange={(event) => setToleranceS(Number(event.target.value))} min="0" max="50" step="1" />
+        </div>
       </label>
       <label>
-        <input type="range" defaultValue={toleranceL} onChange={(event) => setToleranceL(Number(event.target.value))} min="0" max="50" step="1" />
-        {toleranceL}
+        <div className="label">Lightness:</div>
+        <div className="flex">
+          <span className="currentValue">{toleranceL}%</span>
+          <input type="range" defaultValue={toleranceL} onChange={(event) => setToleranceL(Number(event.target.value))} min="0" max="50" step="1" />
+        </div>
       </label>
     </div>
   );
