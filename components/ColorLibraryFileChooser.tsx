f484ec6abc53e8dbe1567ee7789260afac846d82
Ryan Walsh <rcwalsh@gmail.com>
2022-10-19 14:07:19 -0400

    ColorLibraryFileChooser toggleIsVisible

--- components/ColorLibraryFileChooser.tsx
+++ components/ColorLibraryFileChooser.tsx
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { defaultColorsJson } from '../helpers/colors';
 
 const ColorLibraryFileChooser = ({ setLoadedMegaColors }: any) => {
   // https://stackoverflow.com/a/68979570/470749
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  return (
-    <div className="colorLibraryFileChooser">
-      Load from{' '}
-      <a className="jsonLink" href={defaultColorsJson} target="_blank" rel="noreferrer" style={{ display: 'inline-block', maxWidth: '200px', overflow: 'auto' }}>
-        {defaultColorsJson}
-      </a>
-      <input
-        type="file"
-        name="colorLibraryFileChooserInput"
-        multiple={false}
-        onChange={(event: React.FormEvent<HTMLInputElement>) => {
-          const files = Array.from(event.currentTarget.files as FileList);
-          console.log(files);
-          const reader = new FileReader();
-          reader.onload = (onloadEvent: any) => {
-            const txt = onloadEvent?.target?.result; // desired file content https://stackoverflow.com/a/40580004/470749
-            console.log(txt);
-            setLoadedMegaColors(JSON.parse(txt));
-          };
+  function toggleIsVisible() {
+    setIsVisible(!isVisible);
+  }
 
-          // reader.onerror = (error) => reject(error);
-          reader.readAsText(files[0]); // Note that it is also possible to read images and other binaries.
-        }}
-      />
-    </div>
-  );
+  if (isVisible) {
+    return (
+      <div className="colorLibraryFileChooser">
+        Load from{' '}
+        <a className="jsonLink" href={defaultColorsJson} target="_blank" rel="noreferrer" style={{ display: 'inline-block', maxWidth: '200px', overflow: 'auto' }}>
+          {defaultColorsJson}
+        </a>
+        <input
+          type="file"
+          name="colorLibraryFileChooserInput"
+          multiple={false}
+          onChange={(event: React.FormEvent<HTMLInputElement>) => {
+            const files = Array.from(event.currentTarget.files as FileList);
+            console.log(files);
+            const reader = new FileReader();
+            reader.onload = (onloadEvent: any) => {
+              const txt = onloadEvent?.target?.result; // desired file content https://stackoverflow.com/a/40580004/470749
+              console.log(txt);
+              setLoadedMegaColors(JSON.parse(txt));
+            };
+
+            // reader.onerror = (error) => reject(error);
+            reader.readAsText(files[0]); // Note that it is also possible to read images and other binaries.
+          }}
+        />
+        <button onClick={() => toggleIsVisible()}>Hide</button>
+      </div>
+    );
+  } else {
+    return <button onClick={() => toggleIsVisible()}>Load new color library...</button>;
+  }
 };
 
 export default ColorLibraryFileChooser;
